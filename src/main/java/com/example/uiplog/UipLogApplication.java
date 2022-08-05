40024917183a7305346b02a19566074d71b59644
sunshuyang <694536073@qq,com>
2022-08-05 15:39:09 +0800

    提交

---
+++ src/main/java/com/example/uiplog/UipLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.uiplog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UipLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UipLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/uiplog/UipLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uiplog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UipLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
